2f5b583ecc4a6b9776e5d4a9a48ecb6b9d7df509
Ashley Hardin <25ashleyhardin@usd266.com>
2023-12-08 10:22:50 -0600

    did work

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -1,1 +1,46 @@
 import random
+
+def number_guesser():
+    random_number = random.randint(1, 10)
+    guess = ""
+    incorrect_guess = True
+    while incorrect_guess == True:
+        print("guess a number")
+        guess = int(input())
+
+        if guess > random_number:
+            print("too high")
+        elif guess < random_number:
+            print("too low")
+        elif guess == random_number:
+            incorrect_guess = False
+    
+    print("correct")
+
+number_guesser()
+    
+
+def number_guesser_with_lives():
+    lives = 3
+    random_number = random.randint(1, 10)
+    guess = ""
+    incorrect_guess = True
+    while incorrect_guess == True:
+        print("guess a number")
+        guess = int(input())
+
+        if guess > random_number:
+            print("too high")
+            lives = lives - 1
+        elif guess < random_number:
+            print("too low")
+            lives = lives - 1
+        elif lives == 0:
+            print("out of lives")
+            incorrect_guess = False
+        elif guess == random_number:
+            incorrect_guess = False
+    
+    print("correct")
+
+number_guesser_with_lives()
