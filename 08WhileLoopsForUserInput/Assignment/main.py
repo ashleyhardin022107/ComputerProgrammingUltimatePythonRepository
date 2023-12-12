0cb6d4b2c9b070040050bec33867c31547802874
Ashley Hardin <25ashleyhardin@usd266.com>
2023-12-12 09:23:00 -0600

    did work

--- 08WhileLoopsForUserInput/Assignment/main.py
+++ 08WhileLoopsForUserInput/Assignment/main.py
@@ -17,24 +17,27 @@ def number_guesser():
     
     print("correct")
 
-number_guesser()
+# number_guesser()
     
 
 def number_guesser_with_lives():
     lives = 3
+    print("lives: ", lives)
     random_number = random.randint(1, 10)
     guess = ""
     incorrect_guess = True
-    while incorrect_guess == True:
+    while incorrect_guess == True and lives >= 1:
         print("guess a number")
         guess = int(input())
 
         if guess > random_number:
             print("too high")
             lives = lives - 1
+            print("lives: ", lives)
         elif guess < random_number:
             print("too low")
             lives = lives - 1
+            print("lives: ", lives)
         elif lives == 0:
             print("out of lives")
             incorrect_guess = False
@@ -43,4 +46,23 @@ def number_guesser_with_lives():
     
     print("correct")
 
-number_guesser_with_lives()
+#number_guesser_with_lives()
+
+
+def vending_machine():
+    quarter = 25
+    dime = 5
+    nickle = 10
+    amount_due = 50
+    coin_input = ""
+    invalid_coin = True
+    while invalid_coin == True:
+        print("amount due: ", amount_due)
+        coin_input = int(input())
+
+        if coin_input == quarter or coin_input == dime or coin_input == nickle:
+            amount_due = amount_due - coin_input
+            
+
+
+    
